f57c1233a77ca1bc5813acb0116a6379e115cdf8
Robert-Stefan <robertboss05@hotmail.it>
2021-07-10 10:09:27 +0200

    adjust fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    // ADMIN HOMEPAGE
+    public function index() {
+
+        return view('admin.home');
+        
+    }
 }
+
